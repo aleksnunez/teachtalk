c41df0321c50a4ad728242fb3771e9f96b475eb5
Alex Nunez <nunezaleks@gmail.com>
2022-01-10 18:58:53 -0800

    move css from Header to header.style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,30 @@
 import React from 'react'
-import styled from 'styled-components'
-import { Avatar } from "@material-ui/core"
+import SearchIcon from '@material-ui/icons/Search';
 
-function Header() {
+import {HeaderLeft, 
+    HeaderRight, 
+    HeaderContainer, 
+    HeaderAvatar, 
+    HeaderSearch} from './header.style'
+
+const Header = () => {
     return (
         <HeaderContainer>
-            {/* Header Left */}
-            <HeaderLeft>
+            <HeaderLeft> 
+         {/* TODO: create custom svg logo */}      
+            </HeaderLeft>
+            <HeaderSearch>
+                <SearchIcon />
+                <input placeholder='search' />
+            </HeaderSearch>
+            <HeaderRight>
                 <HeaderAvatar // TODO: add on click
                 />
-            </HeaderLeft>
-            {/* Header Search */}
-
-            {/* Header Right */}
-
+                </HeaderRight>
         </HeaderContainer>
     )
 }
 
-export default Header
+export default Header;
 
 // Todo
-const HeaderContainer = styled.div`
-    display: flex;
-    position: fixed;
-    width: 100%;
-    align-items: center;
-    justify-content: space-between;
-    padding: 10px;
-    background-color: var(--teach-color);
-`
-
-const HeaderLeft = styled.div`
-    flex: 0.3;
-    display: flex;
-    align-items: center;
-    margin-left: 20px;
-`
-
-const HeaderAvatar = styled(Avatar)`
-    cursor: pointer;
-
-    :hover {
-        opacity: 0.7;
-    }
-
-`
