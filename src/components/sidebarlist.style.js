2a622cae8a9fbb9d1bdc23e512b8df95bdc50cfc
Alex Nunez <nunezaleks@gmail.com>
2022-01-17 09:50:22 -0800

    move sidebarList styling to separate file

---
+++ src/components/sidebarlist.style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const SidebarListContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-size: small;
+    cursor: pointer;
+    :hover {
+        opacity: 0.7;
+        background-color: #e88873;
+    }
+
+`;
+
+export const SidebarListChannel = styled.div`
+
+`;
