2536fed141bd4b7c44ae85c2fc13a2b77c1be21d
Alex Nunez <nunezaleks@gmail.com>
2022-01-25 09:12:43 -0800

    add Chat component and style

---
+++ src/components/Chat.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ChatContainer } from './chat.style';
+
+const Chat = () => {
+  return  (
+        <ChatContainer>
+            <h1>Hello</h1>
+        </ChatContainer>
+)
+};
+
+export default Chat;
+
---
+++ src/components/chat.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ChatContainer = styled.div`
+flex: 0.7;
+flex-grow: 1;
+overflow-y: scroll;
+margin-top: 50px;
+margin-left: 5px;
+`;
