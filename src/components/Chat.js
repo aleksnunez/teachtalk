4f78ca23b7a8d35ce9822e8fb91980212d0c8d8e
Alex Nunez <nunezaleks@gmail.com>
2022-02-05 08:11:43 -0800

    add chat input component and connect to db

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,12 +1,33 @@
-import { InfoOutlined, School } from '@material-ui/icons';
 import React from 'react';
-import { ChatContainer, Header, HeaderLeft, HeaderRight } from './chat.style';
+import { InfoOutlined, School } from '@material-ui/icons';
+
+import { 
+  ChatContainer, 
+  Header, 
+  HeaderLeft, 
+  HeaderRight, 
+  ChatMessages } from './chat.style';
+  import ChatInput from './ChatInput';
+
 import { useSelector } from 'react-redux';
 import { selectRoomId } from '/Users/alexandernunez/Development/teachtalk/src/features/counter/counterSlice.js'
-import ChatInput from './ChatInput';
+import { useDocument, useCollection } from 'react-firebase-hooks/firestore';
+import { db } from '../firebase';
+
 
 const Chat = () => {
   const roomId = useSelector(selectRoomId);
+  const [roomDetails] = useDocument(
+    roomId && db.collection('rooms').doc(roomId)
+  );
+
+    const [roomMessages] = useCollection(
+      roomId && db.collection('rooms').doc(roomId).collection('messages').orderBy('timestamp', 'asc')
+    )
+
+    console.log(roomDetails?.data());
+    console.log(roomMessages);
+
   return  (
         <ChatContainer>
           <>
@@ -29,12 +50,11 @@ const Chat = () => {
               </HeaderRight>
             </Header>
 
-            {/* <ChatMessages>
+            <ChatMessages>
 
-            </ChatMessages> */}
+            </ChatMessages>
             <ChatInput 
-              //channelName
-              channelId={roomId}
+              channelName={roomDetails?.data().name}
             />
             </>
         </ChatContainer>
--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,17 +1,37 @@
 import {Button}  from '@mui/material';
-import React from 'react';
-import { ChatInputContainer } from './chatinput.style';
+import React, { useState } from 'react';
+import { db } from '../firebase';
+import firebase from 'firebase/compat/app';
+
+import { ChatInputContainer } from "./chatinput.style"
 
 const ChatInput = ({channelName, channelId}) => {
+    const [input, setInput] = useState('');
 
     const sendMessage = e => {
         e.preventDefault();
+
+        console.log(channelId);
+        if(!channelId) {
+            return false;
+        }
+        db.collection('rooms').doc(channelId).collection('messages').add({
+            message: input,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            user: 'Alex',
+            userImage: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fpng&psig=AOvVaw0rWKRHfLXn0XTE50eWjirt&ust=1644100273304000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOCX5_iM5_UCFQAAAAAdAAAAABAD'
+        })
+        setInput('');
     }
 
   return (
     <ChatInputContainer>
         <form>
-            <input placeholder={`Message #$ROOM`}/>
+            <input
+            value={input}
+            onChange={e => setInput(e.target.value)} 
+            placeholder={`Message #$ROOM`}/>
+
             <Button hidden type='submit' onClick={sendMessage}>
                 send
             </Button>
@@ -21,3 +41,4 @@ const ChatInput = ({channelName, channelId}) => {
 };
 
 export default ChatInput;
+
